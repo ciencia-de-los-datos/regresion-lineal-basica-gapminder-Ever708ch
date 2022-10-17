0a4150f973f0a8fbff233b5a74c4f96bb2e01370
Ever Herrera Ríos <111699378+Ever708ch@users.noreply.github.com>
2022-10-17 12:41:27 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -117,13 +117,13 @@ def pregunta_04():
     from sklearn.metrics import mean_squared_error
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv(gm_2008_region.csv)
+    df = pd.read_csv("gm_2008_region.csv")
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = df["fertility"]
+    X_fertility = df["fertility"].values.reshape(-1,1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = df["life"]
+    y_life = df["life"].values.reshape(-1,1)
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
